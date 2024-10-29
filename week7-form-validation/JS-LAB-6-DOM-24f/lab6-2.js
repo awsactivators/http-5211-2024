a98f4ba7392a8dbc15dc27117a7d5a1e9429706f
Genevieve <awavieve@gmail.com>
2024-10-28 21:20:21 -0400

    added week 7 files

---
+++ week7-form-validation/JS-LAB-6-DOM-24f/lab6-2.js
@@ -0,0 +1,42 @@
+﻿//#### LAB 6 - DOM MANIPULATION ####
+//PART 2: MYSTERY BOX
+
+//LISTEN FOR load EVENT
+window.onload = loadEvent;
+
+//'WRAPPER' FUNCTION FOR DOM LOGIC
+function loadEvent() {
+	//alert("Test");
+	//GET DOM ELEMENTS WE'LL NEED
+	var mysteryBox = document.getElementById("mysteryBox");
+	var imageClick = document.getElementById("buttonBox");
+	
+
+
+	//====CREATE THE FUNCTIONS WE'LL NEED====
+	//FUNCTION TO ASK USER
+	function askUser(){
+		var message = confirm("Do you want to see something?");
+		if (message === true){
+			mysteryBox.style.display = "none";
+			imageClick.style.display = "block";
+		} 
+	}
+	
+	//FUNCTION TO CHANGE buttonBox
+	function changeButton() {
+		imageClick.style.width = "600px";
+		imageClick.style.background = "orange";
+	  imageClick.innerHTML = "<h2>SURPRISE!!</h2>";
+	}
+
+
+
+	//SETUP LISTENERS
+	mysteryBox.onmouseover =  askUser;
+	imageClick.onclick = changeButton;
+
+}
+
+
+//END onload FUNCTION
---
+++ week7-form-validation/Sup-8-Forms-2016/8-forms-phone.js
@@ -0,0 +1,68 @@
+﻿/* SUPPLEMENTAL 8:  FORM HANDLING */
+//#1 ========
+window.onload = pageLoaded;
+function pageLoaded(){
+var msgOut = document.getElementById("confirmMsg");
+
+//#2 ========
+var formHandle = document.forms.f_quote;
+formHandle.onsubmit = processForm;
+
+function processForm(){
+//#3 -- !!!!! LOOK FURTHER DOWN THE PAGE FOR #3 !!!!!
+//#4 ========
+	var name_f = formHandle.f_name;
+	var phone_f = formHandle.f_phone;
+	var time_f = formHandle.f_daytime;
+
+//#5 ========
+	var name_span = document.getElementById("msg_name");
+	var phone_span = document.getElementById("msg_phone");
+	var time_span = document.getElementById("msg_time");
+//#6 -- !!!!! LOOK FURTHER DOWN THE PAGE FOR #6 !!!!!
+
+//#7 ========
+//VALIDATE NAME INPUT
+	if (name_f.value === ""){
+		name_f.style.background = "red";
+		name_f.focus();//PUT CURSOR IN THE NAME BOX
+		return false;//PREVENT FORM FROM SUBMITTING
+	} else {
+		name_f.style.background = "white";
+	}
+
+//#8 ========
+//VALIDATE PHONE INPUT
+	var phoneRegEx = /^\d{3}(\s|-)?\d{3}(\s|-)?\d{4}$/;
+//ABOVE MEANS 3 DIGITS; FOLLOWED BY 0 OR 1 SPACE OR DASH; FOLLOWED BY 3 DIGITS; FOLLOWED BY 0 OR 1 SPACE OR DASH; FOLLOWED BY 4 DIGITS. ALL WITH NOTHING ELSE BEFORE OR AFTER THAT COMBINATION OF CHARACTERS.
+	if (phoneRegEx.test(phone_f.value)){//TEST IF INPUT MATCHES OUR REGULAR EXPRESSION.
+		phone_f.style.background = "white";
+	} else {
+		phone_f.value = "";//EMPTY FIELD OF INVALID INPUT
+		phone_f.style.background = "red";
+		phone_f.focus();//PUT CURSOR IN THE PHONE BOX
+		return false;//PREVENT FORM FROM SUBMITTING
+	}
+
+//#6 ========
+	name_span.innerHTML = name_f.value;
+	phone_span.innerHTML = phone_f.value;
+
+//==== EXTRA CHALLENGE ==============
+	var timeMsg;//CREATE MESSAGE VARIABLE
+	if (time_f.checked === true){//WAS THE BOX CHECKED?
+		timeMsg = "after";
+	} else {
+		timeMsg = "during";
+	}
+
+	//CREATE FULL MESSAGE
+	time_span.innerHTML = timeMsg + " business hours";
+//==== END EXTRA CHALLENGE ==========
+
+//#3 ========
+	formHandle.style.display = "none";
+	msgOut.style.display = "block";
+	return false;
+}//======== END processForm ========
+}//end onload
